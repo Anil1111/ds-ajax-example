6d620488180eb42ec3d09c8b6b7fcf0db96df332
Lucas Hiago <lucashiago63@gmail.com>
2020-03-16 13:18:16 -0300

    Add new attributes to html elements to display information

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-require('dotenv/config');
-
 function searchUser() {
 
   let ajax = new XMLHttpRequest();
   let username = document.getElementById('username');
   let display = document.getElementById('user');
-  let client_id = process.env.GITHUB_CLIENT_ID;
-  let client_secret = process.env.GITHUB_CLIENT_SECRET;
-  let url = "https://api.github.com/users/"
+  let client_id = "<your-github-client-id>";
+  let client_secret = "<your-github-client-secret>";
+  let url = "http://api.github.com/users/"
   let params = "?client_id=" + client_id + "&client_secret=" + client_secret;
 
   ajax.onreadystatechange = function() {
@@ -25,26 +23,29 @@ function searchUser() {
 }
 
 function formatResponse(response, display) {
+
   display.innerHTML = "<p></p>"
 
   let user = JSON.parse(response);
   let login = document.createElement("p");
   let profile = document.createElement("img");
-  let name = document.createElement("p");
+  let name = document.createElement("h1");
   let bio = document.createElement("p");
-  let email = document.createElement("p");
+  let link = document.createElement("a");
 
   login.appendChild(document.createTextNode(user.login));
+  login.setAttribute("class", "title");
   profile.setAttribute("src", user.avatar_url);
   name.appendChild(document.createTextNode(user.name));
   bio.appendChild(document.createTextNode(user.bio));
-  email.appendChild(document.createTextNode(user.email));
+  link.setAttribute("href", user.html_url);
+  link.appendChild(document.createTextNode('Link to Github'));
 
   display.appendChild(profile);
-  display.appendChild(login);
   display.appendChild(name);
+  display.appendChild(login);
   display.appendChild(bio);
-  display.appendChild(email);
+  display.appendChild(link);
 
   return display
 }
